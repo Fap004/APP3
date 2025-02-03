3a0bb6fda5c34c9425a6d9f1d1bb294f2a031dd9
eliott-ai <eliott.st-pierre@usherbrooke.ca>
2025-02-03 12:19:09 -0500

    addition csv et débur 1 fonction

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -2,4 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #1. Chargez les données des deux fichiers en mémoire à l’aide de la librairie Numpy.
-np.genfromtxt("")
+np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Primaire.csv', delimiter=',')
+np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Secondaire.csv', delimiter=',')
