82f377809c183a5341fc69f4ed0479cd59cf8b02
fap004 <fpainchaud24@gmail.com>
2025-02-03 13:04:00 -0500

    tableau

--- PythonProject/FAP1582-STPE6468.py
+++ PythonProject/FAP1582-STPE6468.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #1. Chargez les données des deux fichiers en mémoire à l’aide de la librairie Numpy.
-Tableau1 = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Primaire.csv', delimiter=',')
-Tableau2 = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Secondaire.csv', delimiter=',')
-print(Tableau1)
-print(Tableau2)
+Tableau1str = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Primaire.csv',str, delimiter='\n')
+#Tableau2str = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Secondaire.csv',str, delimiter='\n')
+
+Tableau1 = np.genfromtxt(Tableau1str,int,delimiter=',')
+#Tableau2 = np.genfromtxt(Tableau2str,int,delimiter=',')
+print(Tableau1[1][2])
+#print(Tableau2)
