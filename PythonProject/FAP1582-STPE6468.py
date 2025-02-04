d9ead533ddfadd455c4c87c67923b63f13c9c77f
eliott-ai <eliott.st-pierre@usherbrooke.ca>
2025-02-04 11:25:45 -0500

    création de l'histogramme en base log10

--- PythonProject/FAP1582-STPE6468.py
+++ PythonProject/FAP1582-STPE6468.py
@@ -2,21 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#1. Chargez les données des deux fichiers en mémoire à l’aide de la librairie Numpy.
-Tableau1str = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Primaire.csv',str, delimiter='\n')
-#Tableau2str = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Secondaire.csv',str, delimiter='\n')
+#1. Lecture des données des deux .csv en mémoire avEc Numpy.
+def lecture_donnees(nfichier1,nfichier2) :
+    tableau1 = np.genfromtxt(nfichier1,delimiter=',')
+    tableau2 = np.genfromtxt(nfichier2,delimiter=',')
+    return tableau1, tableau2
+#2.+ 3. création dun premier 1er hist et nom axes et titre
+def hist1():
+    #la 1ere ligne est un tuple à cause des multiples return
+    histogramme1, histinutile = lecture_donnees('S2GE_APP3_Problematique_Detecteur_Primaire.csv','S2GE_APP3_Problematique_Detecteur_Secondaire.csv')    #histinutile car on regarde que le 1er
+    plt.figure()
+    plt.xlim(0,300)
+    plt.ylim(0,3000)
+                                                # test:plt.plot(histogramme[:,2]) (ce n'est pas la façon de faire)
+    plt.hist(histogramme1[:,2],bins=1000)       # on fait un tableau que pour le 1er histogramme
+    plt.grid()
+    plt.show()
 
-Tableau1 = np.genfromtxt(Tableau1str,int,delimiter=',')
-#Tableau2 = np.genfromtxt(Tableau2str,int,delimiter=',')
-print(Tableau1[1][2])
-#print(Tableau2)
+def hist2():
+    histogramme1,histogramme2 = lecture_donnees('S2GE_APP3_Problematique_Detecteur_Primaire.csv', 'S2GE_APP3_Problematique_Detecteur_Secondaire.csv')
+    hist1corrige= np.log10(histogramme1[:,2])
+    hist2corrige = np.log10(histogramme2[:,2])
+
+    plt.figure()
+    #plt.xlim(0,300)
+    #plt.ylim(0,3000)       plus beau sans les limites
+                                                # pas utile : plt.plot(histogramme) car on fait l'histogramme
+    plt.title('histogramme amplitude')
+    plt.xlabel('amplitude(mV)')
+    plt.ylabel('quantité')
+    plt.hist(hist1corrige)    #plt.hist(hist1corrige,bins=1000) pas de bins est plus beau
+    plt.hist(hist2corrige)
+    plt.grid()
+    plt.show()
 
 
-plt.figure()
-#x=np.arange(0,1000,0.2)
-plt.plot(Tableau1[:,2])
-plt.xlabel("temps(ms)")
-plt.ylabel("tension(mV)")
-plt.hist()
-plt.grid()
-plt.show()
+hist2()
+
