995de33cfb306e96ec6f4e766f538f6b0e002472
fap004 <fpainchaud24@gmail.com>
2025-02-04 10:06:44 -0500

    tableau

--- PythonProject/FAP1582-STPE6468.py
+++ PythonProject/FAP1582-STPE6468.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 #1. Chargez les données des deux fichiers en mémoire à l’aide de la librairie Numpy.
 Tableau1str = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Primaire.csv',str, delimiter='\n')
 #Tableau2str = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Secondaire.csv',str, delimiter='\n')
@@ -9,3 +10,13 @@ Tableau1 = np.genfromtxt(Tableau1str,int,delimiter=',')
 #Tableau2 = np.genfromtxt(Tableau2str,int,delimiter=',')
 print(Tableau1[1][2])
 #print(Tableau2)
+
+
+plt.figure()
+#x=np.arange(0,1000,0.2)
+plt.plot(Tableau1[:,2])
+plt.xlabel("temps(ms)")
+plt.ylabel("tension(mV)")
+plt.hist()
+plt.grid()
+plt.show()
