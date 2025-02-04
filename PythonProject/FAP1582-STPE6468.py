f2dd035842a6170880aa67b82bd1bc405d5f8d54
eliott-ai <eliott.st-pierre@usherbrooke.ca>
2025-02-04 12:57:13 -0500

    commentaire

--- PythonProject/FAP1582-STPE6468.py
+++ PythonProject/FAP1582-STPE6468.py
@@ -7,7 +7,7 @@ def lecture_donnees(nfichier1,nfichier2) :
     tableau1 = np.genfromtxt(nfichier1,delimiter=',')
     tableau2 = np.genfromtxt(nfichier2,delimiter=',')
     return tableau1, tableau2
-#2.+ 3. création dun premier 1er hist et nom axes et titre
+#2. test histogramme logarithmique
 def hist1():
     #la 1ere ligne est un tuple à cause des multiples return
     histogramme1, histinutile = lecture_donnees('S2GE_APP3_Problematique_Detecteur_Primaire.csv','S2GE_APP3_Problematique_Detecteur_Secondaire.csv')    #histinutile car on regarde que le 1er
@@ -19,12 +19,13 @@ def hist1():
     plt.grid()
     plt.show()
 
+#3.+ 4.+ 5. histogramme complété
 def hist2():
     histogramme1,histogramme2 = lecture_donnees('S2GE_APP3_Problematique_Detecteur_Primaire.csv', 'S2GE_APP3_Problematique_Detecteur_Secondaire.csv')
     hist1corrige= histogramme1[:,2]
     hist2corrige =histogramme2[:,2]
-    bins = np.logspace(np.log10(1), np.log10(1000), num=50)
-
+    bins = np.logspace(1,3, num=25)
+    print(bins)
     plt.figure()
     #plt.xlim(0,300)
     #plt.ylim(0,3000)       plus beau sans les limites
@@ -33,8 +34,9 @@ def hist2():
     plt.xlabel('amplitude(mV)')
     plt.ylabel('quantité')
     plt.semilogx()
-    plt.hist(hist1corrige,bins=bins,histtype="step")    #plt.hist(hist1corrige,bins=1000) pas de bins est plus beau
-    plt.hist(hist2corrige,bins=bins,histtype="step")
+    plt.hist(hist1corrige,bins=bins,histtype='step',label='histogramme primaire')    #plt.hist(hist1corrige,bins=1000) pas de bins est plus beau
+    plt.hist(hist2corrige,bins=bins,histtype='step',label='histogramme secondaire')
+    plt.legend()
     plt.grid()
     plt.show()
 
