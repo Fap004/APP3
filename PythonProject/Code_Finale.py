1f04514732966ceaaa4d99cd3ba618f22bdfd38e
fap004 <fpainchaud24@gmail.com>
2025-02-04 21:41:01 -0500

    ajout fonction print

--- PythonProject/Code_Finale.py
+++ PythonProject/Code_Finale.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from FAP1582_STPE6468 import concidence
 
 temps_mort=0
 fichier=0
 
 
 if temps_mort:
-    plt.figure()
+    plt.errorbar()
     if fichier:
         plt.savefig("PAIF1582,STPE6468--corrige")
+if fichier:
+        plt.savefig("PAIF1582,STPE6468")
+
